0d21b7a930b65ddfd71a687385afbb1d5203adbd
Marat Tlepberghenov <mur.marat@gmail.com>
2018-09-20 15:55:15 +0300

    Prepared ui elements to Rx

--- app/src/main/java/ru/geekbrains/android3_1/task1a/Task1aActivity.java
+++ app/src/main/java/ru/geekbrains/android3_1/task1a/Task1aActivity.java
@@ -1,14 +1,34 @@
 package ru.geekbrains.android3_1.task1a;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.jakewharton.rxbinding.widget.RxTextView;
 import ru.geekbrains.android3_1.R;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class Task1aActivity extends AppCompatActivity {
+
+  @BindView(R.id.task1a_edit_text) EditText editText;
+  @BindView(R.id.task1a_text_view) TextView textView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_task1a);
+    ButterKnife.bind(this);
   }
+
+  Subscription editTextSub = RxTextView
+      .textChanges(editText)
+      .subscribeOn(Schedulers.io())
+      .observeOn(AndroidSchedulers.mainThread())
+      .subscribe(charSequence -> {
+        // do something
+      });
 }
